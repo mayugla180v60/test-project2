9b8d2c46e3d387111b848b92221f4c29d9569a9f
mayu3104 <kami3104@gmail.com>
2025-05-12 23:14:17 +0900

    メール認証実装 修正

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,6 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail 
 {
-//     use Notifiable;
-// }
-
-// class User extends Authenticatable
-// {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
